84c36583d877e61857377d901f10b8da9a4b7f64
yasasArt <yasasjayaweera945@gmail.com>
2025-10-23 23:00:36 +0530

    small changes

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,6 +1,8 @@
 import User from "../models/User.js";
 import bcrypt from "bcrypt"
 import jwt from "jsonwebtoken"
+import dotenv from "dotenv"
+dotenv.config()
 
 export function createUser(req, res) {
 
