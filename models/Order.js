3ed0d5bfb7736d809f79ea65ac53fa300a85ffff
yasasArt <yasasjayaweera945@gmail.com>
2025-12-12 08:23:49 +0530

    oder controller

---
+++ models/Order.js
@@ -0,0 +1,73 @@
+import mongoose from "mongoose";
+
+const orderSchema = new mongoose.Schema(
+    {
+        orderId :{
+            type :  String,
+            required : true,
+            unique : true
+        },
+        email :{
+            type : String,
+            required : true 
+        },
+    name:{
+        type : String,
+        required :true
+    },
+    address :{
+        type :String,
+        required :true
+    },
+    date :{
+        type :Date,
+        required :true,
+        default :Date.now
+    },
+    total :{
+        type : Number,
+        required :true
+    },
+    status :{
+        type :String,
+        required :true,
+        default : "pending"
+    },
+    phone : {
+        type : String,
+        required : false,
+    },
+    notes : {
+        type :String,
+        required : false
+    },
+    items :[
+        {
+            productID :{
+                type :String,
+                required :true
+            },
+            name:{
+                type: String,
+                required: true
+            },
+            price : {
+                type : Number,
+                required:true
+            },
+            quantity : {
+                type :Number,
+                required: true
+            },
+            image: {
+                type: String,
+                required: true
+            }
+        }
+    ]
+    }
+)
+
+const Order = mongoose.model("order", orderSchema)
+
+export default Order;
