b018efff8849d59f0da018df8a6676069f3d4d74
yasasArt <yasasjayaweera945@gmail.com>
2025-12-15 18:08:07 +0530

    google-login

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,7 @@ import User from "../models/User.js";
 import bcrypt from "bcrypt"
 import jwt from "jsonwebtoken"
 import dotenv from "dotenv"
+import axios from "axios";
 dotenv.config()
 
 export function createUser(req, res) {
@@ -15,7 +16,6 @@ export function createUser(req, res) {
         firstName : data.firstName,
         lastName : data.lastName,
         password : hashedPassword,
-        role : data.role,
     }) // aluthin ena userwa user kenek widiyta hadgnnwa
 
 
@@ -98,4 +98,80 @@ export function getUser(req,res){
         return
     }
     res.json(req.user)
+}
+
+export async function googleLogin(req, res) {
+  try {
+    const response = await axios.get(
+      "https://www.googleapis.com/oauth2/v3/userinfo",
+      {
+        headers: {
+          Authorization: `Bearer ${req.body.token}`,
+        },
+      }
+    );
+
+    const existingUser = await User.findOne({ email: response.data.email });
+
+    if (existingUser == null) {
+      const hashedPassword = bcrypt.hashSync("google-login", 10);
+
+      const newUser = new User({
+        email: response.data.email,
+        firstName: response.data.given_name,
+        lastName: response.data.family_name,
+        password: hashedPassword,
+        image: response.data.picture,
+        isEmailVerified: true,
+      });
+
+      await newUser.save();
+
+      const payload = {
+        email: newUser.email,
+        firstName: newUser.firstName,
+        lastName: newUser.lastName,
+        role: newUser.role,
+        isEmailVerified: true,
+        image: newUser.image,
+      };
+
+      const token = jwt.sign(payload, process.env.JWT_SECRET, {
+        expiresIn: "150h",
+      });
+
+      return res.json({
+        message: "Login successful",
+        token: token,
+        role: newUser.role,
+      });
+    }
+
+    // ✅ EXISTING USER
+    const payload = {
+      email: existingUser.email,
+      firstName: existingUser.firstName,
+      lastName: existingUser.lastName, // ✅ FIX
+      role: existingUser.role,
+      isEmailVerified: existingUser.isEmailVerified,
+      image: existingUser.image,
+    };
+
+    const token = jwt.sign(payload, process.env.JWT_SECRET, {
+      expiresIn: "150h",
+    });
+
+    res.json({
+      message: "Login successful",
+      token: token,
+      role: existingUser.role,
+    });
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({
+      message: "Google login failed",
+      error: error.message,
+    });
+  }
 }
--- models/User.js
+++ models/User.js
@@ -22,7 +22,7 @@ const userSchema = new mongoose.Schema({
     },
     role:{
         type : String,
-        default : "custommer" 
+        default : "customer" 
     },
     isBlocked : {
         type : Boolean,
